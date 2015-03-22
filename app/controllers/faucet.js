4d2b65cd4493c3f4db10d57c666a69160513430c
Kirill Fomichev <fanatid@ya.ru>
2015-03-22 23:26:22 +0300

    add commonResponse

--- app/controllers/faucet.js
+++ app/controllers/faucet.js
@@ -1,9 +1,19 @@
 var faucet = require('../../lib/faucet').default()
 
+function commonResponse (promise, res) {
+  promise.done(function (data) {
+    res.jsonp({status: 'success', data: data})
+  }, function (err) {
+    res.jsonp({status: 'fail', message: err.message})
+  })
+}
+
 module.exports.unspents = function (req, res) {
-  res.jsonp({unspent: '12e'})
+  var promise = faucet.getUnspents(req.query.type)
+  commonResponse(promise, res)
 }
 
 module.exports.withdrawal = function (req, res) {
-  res.jsonp({txId: 'asd'})
+  var promise = faucet.withdrawal(req.query.toAddress, req.query.value)
+  commonResponse(promise, res)
 }
