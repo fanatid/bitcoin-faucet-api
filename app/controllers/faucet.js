b3cac344fd288f6519cc8a69f57aeabedea8b2b8
Kirill Fomichev <fanatid@ya.ru>
2015-03-31 21:10:21 +0300

    improve faucet controller

--- app/controllers/faucet.js
+++ app/controllers/faucet.js
@@ -1,22 +1,21 @@
 var faucet = require('../../lib/faucet').default()
 var logger = require('../../lib/logger').logger
 
-// split on two functions: onFulfilled, onRejected...
-function commonResponse (promise, res) {
-  promise.done(function (data) {
-    res.jsonp({status: 'success', data: data})
-  }, function (err) {
-    logger.error('faucet error: ', err)
-    res.jsonp({status: 'fail', message: err.message})
-  })
+function onFulfilled (data) {
+  this.jsonp({status: 'success', data: data})
+}
+
+function onRejected (err) {
+  logger.error('faucet error: ', err)
+  this.jsonp({status: 'fail', message: err.message})
 }
 
 module.exports.preload = function (req, res) {
-  var promise = faucet.getUnspents(req.query.type)
-  commonResponse(promise, res)
+  faucet.getUnspents(req.query.type)
+    .done(onFulfilled.bind(res), onRejected.bind(res))
 }
 
 module.exports.withdrawal = function (req, res) {
-  var promise = faucet.withdrawal(req.query.toAddress, parseInt(req.query.value))
-  commonResponse(promise, res)
+  faucet.withdrawal(req.query.toAddress, parseInt(req.query.value, 10))
+    .done(onFulfilled.bind(res), onRejected.bind(res))
 }
